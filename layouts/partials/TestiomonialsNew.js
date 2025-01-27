e0896f40f6894456ad3a4a9640546dda22e98b3a
Ashvi <ashvi.dev@gmail.com>
2025-01-28 05:08:57 +0530

    fix

--- layouts/partials/TestiomonialsNew.js
+++ layouts/partials/TestiomonialsNew.js
@@ -99,8 +99,7 @@ const InfiniteTestimonialSlider = () => {
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
     };
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
+   
   }, []);
 
 
